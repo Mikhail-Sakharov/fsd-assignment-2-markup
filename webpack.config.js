d594f042e8e796b1b9858671e75a79030bd8f817
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-08-24 15:16:43 +0300

    feat: добавляет начальные настройки для webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, 'src', 'js', 'index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: []
+  },
+  plugins: []
+}
