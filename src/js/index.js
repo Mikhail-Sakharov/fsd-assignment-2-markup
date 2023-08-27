7d15e4dc4fc3acef80b135d1f06425c08f23a07d
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-08-27 15:06:37 +0300

    feat: добавляет страницу с элементами и элемент текстового поля

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,2 @@
 import '../styles/main.scss';
+import '../pug/ui-kit/includes/text-field/text-field';
---
+++ src/pug/ui-kit/includes/text-field/text-field.js
@@ -0,0 +1,23 @@
+const customInputElements = document.querySelectorAll('.custom-input');
+
+const handleInputFocus = (_evt, inputWrapperElement) => {
+  inputWrapperElement.classList.add('custom-input__wrapper--focused');
+};
+
+const handleInputBlur = (_evt, inputWrapperElement, inputElement) => {
+  inputWrapperElement.classList.remove('custom-input__wrapper--focused');
+};
+
+customInputElements.forEach((customInputElement) => {
+  const inputElement = customInputElement.querySelector('input');
+  const inputWrapperElement = customInputElement.querySelector('.custom-input__wrapper');
+
+  inputElement.addEventListener('focus', (evt) => handleInputFocus(evt, inputWrapperElement));
+});
+
+customInputElements.forEach((customInputElement) => {
+  const inputElement = customInputElement.querySelector('input');
+  const inputWrapperElement = customInputElement.querySelector('.custom-input__wrapper');
+
+  inputElement.addEventListener('blur', (evt) => handleInputBlur(evt, inputWrapperElement, inputElement));
+});
