6eaf30ee614f9009edd6ace803fbfb85a5adcdb4
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-09-06 12:36:00 +0300

    fix: заменяет masked-text-field на custom-text-field и исправляет отображение второго

--- src/pug/ui-kit/includes/custom-text-field/custom-text-field.js
+++ src/pug/ui-kit/includes/custom-text-field/custom-text-field.js
@@ -1,6 +1,4 @@
-const customInputElement = document.querySelector('.custom-text-field');
-const customInputElementDisplay = customInputElement.querySelector('.custom-text-field__display');
-const inputElement = customInputElementDisplay.querySelector('input');
+const customInputElements = document.querySelectorAll('.custom-text-field');
 
 const handleInputFocus = (_evt, inputWrapperElement) => {
   inputWrapperElement.classList.add('custom-text-field__display--focused');
@@ -10,5 +8,16 @@ const handleInputBlur = (_evt, inputWrapperElement) => {
   inputWrapperElement.classList.remove('custom-text-field__display--focused');
 };
 
-inputElement.addEventListener('focus', (evt) => handleInputFocus(evt, customInputElementDisplay));
-inputElement.addEventListener('blur', (evt) => handleInputBlur(evt, customInputElementDisplay));
+customInputElements.forEach((customInputElement) => {
+  const customInputElementDisplay = customInputElement.querySelector('.custom-text-field__display');
+  const inputElement = customInputElementDisplay.querySelector('input');
+
+  inputElement.addEventListener('focus', (evt) => handleInputFocus(evt, customInputElementDisplay));
+});
+
+customInputElements.forEach((customInputElement) => {
+  const customInputElementDisplay = customInputElement.querySelector('.custom-text-field__display');
+  const inputElement = customInputElementDisplay.querySelector('input');
+
+  inputElement.addEventListener('blur', (evt) => handleInputBlur(evt, customInputElementDisplay));
+});
