48d9cee45d60f1bddb4548a861a7859772f1dde6
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-09-08 17:23:00 +0300

    feat: добавляет компонент room-card

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
 import '../styles/main.scss';
 import '../pug/ui-kit/includes/review/image.jpg';
+import '../pug/ui-kit/includes/slider/1.jpg';
+import '../pug/ui-kit/includes/slider/2.jpg';
+import '../pug/ui-kit/includes/slider/3.jpg';
 import '../pug/ui-kit/includes/custom-text-field/custom-text-field';
 import '../pug/ui-kit/includes/subscription-text-field/subscription-text-field';
