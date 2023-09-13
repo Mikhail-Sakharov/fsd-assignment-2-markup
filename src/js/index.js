7d36b124760d46183c5467f90cd3b706034ecc53
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-09-13 16:25:42 +0300

    feat: подключает flatpickr в проект

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 import '../styles/main.scss';
+
 import '../pug/ui-kit/includes/review/image.jpg';
 import '../pug/ui-kit/includes/slider/1.jpg';
 import '../pug/ui-kit/includes/slider/2.jpg';
+import '../pug/pages/landing-bg-3.jpg';
+
 import '../pug/ui-kit/includes/custom-text-field/custom-text-field';
 import '../pug/ui-kit/includes/subscription-text-field/subscription-text-field';
 import '../pug/ui-kit/includes/range-slider/range-slider';
-import '../pug/pages/landing-bg-3.jpg';
+import '../pug/ui-kit/cards/calendar/flatpickr';
