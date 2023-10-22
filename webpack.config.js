47228f20f11bab5a458d19e329c0e587e9ea066e
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-10-22 17:58:36 +0300

    feat: добавляет babel-loader и настройки для него

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.pug$/,
         use: 'pug-loader'
       },
