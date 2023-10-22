fd5ce8823da58a41da7b66a1a1355cb75a01a92a
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-10-22 17:37:37 +0300

    feat: добавляет фоновый слайдер для landing-page

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,9 @@ import '../styles/main.scss';
 import '../pug/ui-kit/includes/review/image.jpg';
 import '../pug/ui-kit/includes/slider/1.jpg';
 import '../pug/ui-kit/includes/slider/2.jpg';
-import '../pug/pages/landing-bg-3.jpg';
+import '../pug/ui-kit/includes/main-page-slider/landing-bg-1.jpg';
+import '../pug/ui-kit/includes/main-page-slider/landing-bg-2.jpg';
+import '../pug/ui-kit/includes/main-page-slider/landing-bg-3.jpg';
 
 import '../pug/ui-kit/includes/custom-text-field/custom-text-field';
 import '../pug/ui-kit/includes/subscription-text-field/subscription-text-field';
