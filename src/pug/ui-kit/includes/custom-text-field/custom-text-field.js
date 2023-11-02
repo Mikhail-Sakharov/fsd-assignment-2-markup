3cc629dbf4a4ba6edab94d1701869277612cf61e
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-11-02 09:43:19 +0300

    feat: добавляет обводку фокуса для like-button только при использовании Tab

--- src/pug/ui-kit/includes/custom-text-field/custom-text-field.js
+++ src/pug/ui-kit/includes/custom-text-field/custom-text-field.js
@@ -2,11 +2,11 @@ import './custom-text-field.scss';
 
 const customInputElements = document.querySelectorAll('.custom-text-field');
 
-const handleInputFocus = (_evt, inputWrapperElement) => {
+const handleInputFocus = (inputWrapperElement) => {
   inputWrapperElement.classList.add('custom-text-field__display--focused');
 };
 
-const handleInputBlur = (_evt, inputWrapperElement) => {
+const handleInputBlur = (inputWrapperElement) => {
   inputWrapperElement.classList.remove('custom-text-field__display--focused');
 };
 
@@ -14,12 +14,12 @@ customInputElements.forEach((customInputElement) => {
   const customInputElementDisplay = customInputElement.querySelector('.custom-text-field__display');
   const inputElement = customInputElementDisplay.querySelector('input');
 
-  inputElement.addEventListener('focus', (evt) => handleInputFocus(evt, customInputElementDisplay));
+  inputElement.addEventListener('focus', () => handleInputFocus(customInputElementDisplay));
 });
 
 customInputElements.forEach((customInputElement) => {
   const customInputElementDisplay = customInputElement.querySelector('.custom-text-field__display');
   const inputElement = customInputElementDisplay.querySelector('input');
 
-  inputElement.addEventListener('blur', (evt) => handleInputBlur(evt, customInputElementDisplay));
+  inputElement.addEventListener('blur', () => handleInputBlur(customInputElementDisplay));
 });
