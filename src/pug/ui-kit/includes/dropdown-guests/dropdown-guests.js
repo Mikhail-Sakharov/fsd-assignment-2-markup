503c47f14eee8e8ce444734b6fa645be227ede75
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-11-04 13:39:20 +0300

    feat: добавляет управление клавишей tab для dropdown-guests

--- src/pug/ui-kit/includes/dropdown-guests/dropdown-guests.js
+++ src/pug/ui-kit/includes/dropdown-guests/dropdown-guests.js
@@ -1,1 +1,24 @@
 import './dropdown-guests.scss';
+
+const dropdownGuestsButtons = document.querySelectorAll('.dropdown-guests__button');
+
+const handleDropdownGuestsButtonFocus = (dropdownGuestsInput) => {
+  dropdownGuestsInput.checked = true;
+};
+
+const handleDropdownGuestsButtonBlur = (dropdownGuestsInput) => {
+  dropdownGuestsInput.checked = false;
+};
+
+dropdownGuestsButtons.forEach((dropDownButton) => {
+  const dropdownGuestsInput = dropDownButton.closest('.dropdown-guests').querySelector('.dropdown-guests__input');
+  const dropdownGuestsTextButtons = dropdownGuestsInput.closest('.dropdown-guests').querySelectorAll('.button-text');
+
+  dropDownButton.addEventListener('focus', () => handleDropdownGuestsButtonFocus(dropdownGuestsInput));
+  dropDownButton.addEventListener('blur', () => handleDropdownGuestsButtonBlur(dropdownGuestsInput));
+
+  dropdownGuestsTextButtons.forEach((textButton) => {
+    textButton.addEventListener('focus', () => handleDropdownGuestsButtonFocus(dropdownGuestsInput));
+    textButton.addEventListener('blur', () => handleDropdownGuestsButtonBlur(dropdownGuestsInput));
+  });
+});
