12fc6cc5f76b4bd8f99421883050911458298176
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-11-04 12:52:50 +0300

    feat: добавляет управление клавишей tab для dropdown

--- src/pug/ui-kit/includes/dropdown/dropdown.js
+++ src/pug/ui-kit/includes/dropdown/dropdown.js
@@ -1,1 +1,17 @@
 import './dropdown.scss';
+
+const dropdownButtons = document.querySelectorAll('.dropdown__button');
+
+const handleDropdownButtonFocus = (dropdownInput) => {
+  dropdownInput.checked = true;
+};
+
+const handleDropdownButtonBlur = (dropdownInput) => {
+  dropdownInput.checked = false;
+};
+
+dropdownButtons.forEach((button) => {
+  const dropdownInput = button.closest('.dropdown').querySelector('.dropdown__input');
+  button.addEventListener('focus', () => handleDropdownButtonFocus(dropdownInput));
+  button.addEventListener('blur', () => handleDropdownButtonBlur(dropdownInput));
+});
