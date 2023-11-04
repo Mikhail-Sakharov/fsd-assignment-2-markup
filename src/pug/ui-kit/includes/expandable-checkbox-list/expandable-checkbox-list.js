88b7a0a692e7922d2fbf18046457d5fc662f5cbf
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-11-04 14:19:47 +0300

    feat: добавляет управление клавишей tab для expandable-checkbox-list

--- src/pug/ui-kit/includes/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/pug/ui-kit/includes/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,1 +1,21 @@
 import './expandable-checkbox-list.scss';
+
+const expandableCheckboxLists = document.querySelectorAll('.expandable-checkbox-list');
+
+const handleExpandableCheckboxListInputFocus = (listInput) => {
+  listInput.checked = true;
+};
+
+const handleExpandableCheckboxListInputBlur = (listInput) => {
+  listInput.checked = false;
+};
+
+expandableCheckboxLists.forEach((list) => {
+  const checkboxInputs = list.querySelectorAll('.custom-checkbox input');
+  const listInput = list.querySelector('.expandable-checkbox-list__input');
+
+  checkboxInputs.forEach((input) => {
+    input.addEventListener('focus', () => handleExpandableCheckboxListInputFocus(listInput));
+    input.addEventListener('blur', () => handleExpandableCheckboxListInputBlur(listInput));
+  });
+});
