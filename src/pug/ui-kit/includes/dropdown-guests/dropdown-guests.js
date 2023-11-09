38f2097163037aa2744974672329a2d969a9b2a6
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-11-09 09:19:00 +0300

    fix: рефакторинг компонента dropdown-guests

--- src/pug/ui-kit/includes/dropdown-guests/dropdown-guests.js
+++ src/pug/ui-kit/includes/dropdown-guests/dropdown-guests.js
@@ -6,19 +6,13 @@ const handleDropdownGuestsButtonFocus = (dropdownGuestsInput) => {
   dropdownGuestsInput.checked = true;
 };
 
-const handleDropdownGuestsButtonBlur = (dropdownGuestsInput) => {
-  dropdownGuestsInput.checked = false;
-};
-
 dropdownGuestsButtons.forEach((dropDownButton) => {
   const dropdownGuestsInput = dropDownButton.closest('.dropdown-guests').querySelector('.dropdown-guests__input');
   const dropdownGuestsTextButtons = dropdownGuestsInput.closest('.dropdown-guests').querySelectorAll('.button-text');
 
   dropDownButton.addEventListener('focus', () => handleDropdownGuestsButtonFocus(dropdownGuestsInput));
-  dropDownButton.addEventListener('blur', () => handleDropdownGuestsButtonBlur(dropdownGuestsInput));
 
   dropdownGuestsTextButtons.forEach((textButton) => {
     textButton.addEventListener('focus', () => handleDropdownGuestsButtonFocus(dropdownGuestsInput));
-    textButton.addEventListener('blur', () => handleDropdownGuestsButtonBlur(dropdownGuestsInput));
   });
 });
