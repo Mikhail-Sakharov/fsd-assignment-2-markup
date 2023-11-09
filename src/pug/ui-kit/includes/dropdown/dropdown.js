9480e16c6bab2ae2418960bba02c6a58f901ec51
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-11-09 09:16:46 +0300

    fix: рефакторинг компонента dropdown

--- src/pug/ui-kit/includes/dropdown/dropdown.js
+++ src/pug/ui-kit/includes/dropdown/dropdown.js
@@ -6,12 +6,7 @@ const handleDropdownButtonFocus = (dropdownInput) => {
   dropdownInput.checked = true;
 };
 
-const handleDropdownButtonBlur = (dropdownInput) => {
-  dropdownInput.checked = false;
-};
-
 dropdownButtons.forEach((button) => {
   const dropdownInput = button.closest('.dropdown').querySelector('.dropdown__input');
   button.addEventListener('focus', () => handleDropdownButtonFocus(dropdownInput));
-  button.addEventListener('blur', () => handleDropdownButtonBlur(dropdownInput));
 });
