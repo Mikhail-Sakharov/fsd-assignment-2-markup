cf67019a67809f68f02a6adcf5f5573ca218505a
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-11-09 09:13:25 +0300

    fix: рефакторинг компонента expandable-checkbox-list

--- src/pug/ui-kit/includes/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/pug/ui-kit/includes/expandable-checkbox-list/expandable-checkbox-list.js
@@ -2,20 +2,15 @@ import './expandable-checkbox-list.scss';
 
 const expandableCheckboxLists = document.querySelectorAll('.expandable-checkbox-list');
 
-const handleExpandableCheckboxListInputFocus = (listInput) => {
-  listInput.checked = true;
-};
-
-const handleExpandableCheckboxListInputBlur = (listInput) => {
-  listInput.checked = false;
+const handleExpandableCheckboxListInputFocus = (showHideListInput) => {
+  showHideListInput.checked = true;
 };
 
 expandableCheckboxLists.forEach((list) => {
-  const checkboxInputs = list.querySelectorAll('.custom-checkbox input');
-  const listInput = list.querySelector('.expandable-checkbox-list__input');
+  const showHideListInput = list.querySelector('.expandable-checkbox-list__input');
+  const checkboxInputs = list.querySelectorAll('.custom-checkbox__input');
 
   checkboxInputs.forEach((input) => {
-    input.addEventListener('focus', () => handleExpandableCheckboxListInputFocus(listInput));
-    input.addEventListener('blur', () => handleExpandableCheckboxListInputBlur(listInput));
+    input.addEventListener('focus', () => handleExpandableCheckboxListInputFocus(showHideListInput));
   });
 });
