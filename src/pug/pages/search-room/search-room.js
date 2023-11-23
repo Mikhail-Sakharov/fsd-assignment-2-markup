dc2f66097fd823875c275d999fb9c094a936f1bd
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-11-23 19:36:34 +0300

    fix: рефакторинг страницы search-room

--- src/pug/pages/search-room/search-room.js
+++ src/pug/pages/search-room/search-room.js
@@ -1,1 +1,7 @@
 import './search-room.scss';
+
+const customSelectButtonElement = document
+  .querySelector('.room-list__container')
+  .querySelector('.custom-select__button');
+
+customSelectButtonElement.focus();
