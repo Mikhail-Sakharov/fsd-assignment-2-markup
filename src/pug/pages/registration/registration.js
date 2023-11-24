93bc127ee8e9842f16112f23660461f521383797
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-11-24 07:30:17 +0300

    fix: рефакторинг страницы registration

--- src/pug/pages/registration/registration.js
+++ src/pug/pages/registration/registration.js
@@ -1,1 +1,7 @@
 import './registration.scss';
+
+const registrationPageFocusedElement = document
+  .querySelector('.registration__form')
+  ?.querySelector('.custom-text-field__input');
+
+registrationPageFocusedElement?.focus();
