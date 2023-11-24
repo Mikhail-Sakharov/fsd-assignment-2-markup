4d71eabe5f3a7a849693534db7d9c2ff48413f63
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-11-24 07:38:29 +0300

    fix: рефакторинг страницы sign-in

--- src/pug/pages/sign-in/sign-in.js
+++ src/pug/pages/sign-in/sign-in.js
@@ -1,1 +1,7 @@
 import './sign-in.scss';
+
+const signInPageFocusedElement = document
+  .querySelector('.sign-in__form')
+  ?.querySelector('.custom-text-field__input');
+
+signInPageFocusedElement?.focus();
